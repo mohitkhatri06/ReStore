c9e81b4b77ac331bdfe683a39c4be2d85dba2994
mohit <mohitkhatri4272@gmail.com>
2022-08-23 13:08:40 +0530

    Home with Material UI updated

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddDbContext<StoreContext> (opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -42,6 +44,11 @@ catch (Exception ex)
 
 
 app.UseHttpsRedirection();
+
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
 app.UseAuthorization();
 
